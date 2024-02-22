dd276ccad9f74a84bb7cf5b21c715490a967a54d
Audrey Norris <61563331+Audrey-Norris@users.noreply.github.com>
2024-02-22 15:48:25 -0700

    Created ability to make recipes

--- Assets/Scripts/CraftingManager.cs
+++ Assets/Scripts/CraftingManager.cs
@@ -6,7 +6,40 @@ public class CraftingManager : MonoBehaviour
 {
     public Recipe recipe;
 
+    public List<ItemList> inputMats;
+
     public void Start() {
-        Debug.Log(recipe);
+
+    }
+
+    public void addToList(Material mat) { 
+        
+    }
+
+    public void createRecipe() {
+        bool goodRecipe = true;
+        List<ItemList> mats = recipe.getMaterials();
+        foreach(var mat in mats) {
+            Debug.Log(mat.mat +" " + mat.amount);
+            foreach (var input in inputMats) {
+                Debug.Log(input.mat + " " + input.amount);
+                if (input.mat == mat.mat & input.amount == mat.amount) {
+                    goodRecipe = true;
+                    break;
+                } else {
+                    goodRecipe = false;
+                }
+                Debug.Log("Ingredient Correct!");
+            }
+           if (!goodRecipe) {
+                break;     
+           }
+        }
+
+        if(!goodRecipe) {
+            Debug.Log("Recipe BAD!");
+        } else {
+            Debug.Log("Recipe Complete!" + recipe);
+        }
     }
 }
