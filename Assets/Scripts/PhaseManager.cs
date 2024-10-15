5cf0f0cae67908abeaa72ac2e7427a8ab6dd86ab
Audrey Norris <61563331+Audrey-Norris@users.noreply.github.com>
2024-10-14 23:02:50 -0600

    added script and moved item script

---
+++ Assets/Scripts/PhaseManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DayStates { Upkeep, Chore, Celebration};
+
+public class PhaseManager : MonoBehaviour
+{
+    
+}
