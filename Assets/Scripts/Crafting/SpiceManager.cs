764b5fb04f2a1b47a3eac8f01d01d8cfc0fec947
Audrey Norris <61563331+Audrey-Norris@users.noreply.github.com>
2025-03-23 15:16:27 -0600

    Spices can move to the bowl and will affect the slider values

--- Assets/Scripts/Crafting/SpiceManager.cs
+++ Assets/Scripts/Crafting/SpiceManager.cs
@@ -17,11 +17,16 @@ public class SpiceManager : MonoBehaviour
 
     [SerializeField] private SliderManager sliders;
 
+
+    [SerializeField] private int spicy = 0, sweet = 0, bitter = 0, sour = 0, savory = 0;
+
     public void PopulateSpices() {
         inventory = GameObject.Find("SaveManager").GetComponent<InventoryManager>();
         ItemList[] spices = inventory.GetAllItems();
 
         foreach (ItemList spice in spices) {
+            Debug.Log(spice.item.name);
+            Debug.Log(spice.total);
             if(spice.item.getItemType() == ItemType.Spice) {
                 CreateSpiceObject(spice, spiceArea, spiceObjects);
             }
@@ -30,7 +35,7 @@ public class SpiceManager : MonoBehaviour
 
     public void CreateSpiceObject(ItemList info, GameObject parent , List<GameObject> location) {
         GameObject newSpice = Instantiate(itemPrefab, parent.transform);
-        itemPrefab.GetComponent<ItemInfo>().LoadItemInfo(info, this.gameObject);
+        newSpice.GetComponent<ItemInfo>().LoadItemInfo(info, this.gameObject);
         location.Add(newSpice);
     }
 
@@ -38,16 +43,19 @@ public class SpiceManager : MonoBehaviour
         ItemList itemInfo = spice.GetComponent<ItemInfo>().itemInfo;
         GameObject location;
         List<GameObject> listLocation;
-        if (spice.transform.parent == spiceArea) {
+        //Determines the location the item is going to
+        if (spice.transform.parent.name == spiceArea.name) {
             location = activeSpiceArea;
             listLocation = activeSpiceObjects;
+
         } else {
             location = spiceArea;
             listLocation = spiceObjects;
         }
 
         if (itemInfo.total > 1) { //If there is greater than 1 spice objects create a new object
-            ItemList newItem = new ItemList(itemInfo.item, itemInfo.total);
+            Debug.Log("Running!");
+            ItemList newItem = new ItemList(itemInfo.item, 1);
             CreateSpiceObject(newItem, location, listLocation);
             itemInfo.total--;
             spice.GetComponent<ItemInfo>().UpdateItemInfo(itemInfo);
@@ -69,8 +77,55 @@ public class SpiceManager : MonoBehaviour
 
     }
 
+    //Takes the entire list of spices currently active and calculates the spice value of the dish
     public void UpdateValues() {
-
+        spicy = 0;
+        sweet = 0;
+        bitter = 0;
+        savory = 0;
+        sour = 0;
+        if(activeSpiceObjects.Count != 0) {
+            foreach (GameObject x in activeSpiceObjects) {
+                Spices spice = (Spices)x.GetComponent<ItemInfo>().itemInfo.item;
+                foreach (Flavor flavor in spice.flavorList) {
+                    switch (flavor.name) {
+                        case "Spicy": //Spicy reduces sweetness
+                            spicy += flavor.total;
+                            sweet -= Mathf.FloorToInt(flavor.total / 3);
+                            break;
+                        case "Sweet": //Sweet enchances umami and reduces spicy and bitter
+                            sweet += flavor.total;
+                            savory += Mathf.FloorToInt(flavor.total / 3);
+                            spicy -= Mathf.FloorToInt(flavor.total / 3);
+                            bitter -= Mathf.FloorToInt(flavor.total / 3);
+                            break;
+                        case "Bitter": //Bitter reduces savory and sweet
+                            bitter += flavor.total;
+                            savory -= Mathf.FloorToInt(flavor.total / 3);
+                            sweet -= Mathf.FloorToInt(flavor.total / 3);
+                            break;
+                        case "Sour": //Sour reduces bitter and spice and sweet
+                            sour += flavor.total;
+                            spicy -= Mathf.FloorToInt(flavor.total / 3);
+                            bitter -= Mathf.FloorToInt(flavor.total / 3);
+                            sweet -= Mathf.FloorToInt(flavor.total / 3);
+                            break;
+                        case "Umami": //Savory enhances sweet reduces bitter
+                            savory += flavor.total;
+                            sweet += Mathf.FloorToInt(flavor.total / 3);
+                            bitter -= Mathf.FloorToInt(flavor.total / 3);
+                            break;
+                    }
+                }
+            }
+        }
+        
+        //Updates slider values
+        sliders.AddFlavor("Spicy", spicy);
+        sliders.AddFlavor("Sweet", sweet);
+        sliders.AddFlavor("Umami", savory);
+        sliders.AddFlavor("Sour", sour);
+        sliders.AddFlavor("Bitter", bitter);
     }
 
     public void CraftItems() {
--- Assets/SliderManager.cs
+++ Assets/SliderManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 [System.Serializable]
 public class FoodSlider {
     public Slider slider;
+    public TMP_Text text;
     public Flavor flavor;
 }
 
@@ -25,48 +27,11 @@ public class SliderManager : MonoBehaviour {
         return null;
     }
 
-    //This takes the list of spices currently in the bowl and calculates the spice value of the sliders and updates it accordingly
-    public void UpdateFlavors(List<GameObject> spiceObjects) {
-        int spicy = 0, sweet = 0, bitter = 0, sour = 0, savory = 0;
-        foreach(GameObject x in spiceObjects) {
-            Spices spice = (Spices)x.GetComponent<ItemInfo>().itemInfo.item;
-            foreach(Flavor flavor in spice.flavorList) {
-                switch(flavor.name) {
-                    case "Spicy": //Spicy reduces sweetness
-                        spicy += flavor.total;
-                        sweet -= Mathf.RoundToInt(flavor.total/3);
-                        break;
-                    case "Sweet": //Sweet enchances umami and reduces spicy and bitter
-                        sweet += flavor.total;
-                        savory += Mathf.RoundToInt(flavor.total / 3);
-                        spicy -= Mathf.RoundToInt(flavor.total / 3);
-                        bitter -= Mathf.RoundToInt(flavor.total / 3);
-                        break;
-                    case "Bitter": //Bitter reduces savory and sweet
-                        bitter += flavor.total;
-                        savory -= Mathf.RoundToInt(flavor.total / 3);
-                        sweet -= Mathf.RoundToInt(flavor.total / 3);
-                        break;
-                    case "Sour": //Sour reduces bitter and spice and sweet
-                        sour += flavor.total;
-                        spicy -= Mathf.RoundToInt(flavor.total / 3);
-                        bitter -= Mathf.RoundToInt(flavor.total / 3);
-                        sweet -= Mathf.RoundToInt(flavor.total / 3);
-                        break;
-                    case "Umami": //Savory enhances sweet reduces bitter
-                        savory += flavor.total;
-                        sweet += Mathf.RoundToInt(flavor.total / 3);
-                        bitter -= Mathf.RoundToInt(flavor.total / 3);
-                        break;
-                }
-            }
-        }
-    }
-
-    public void AddFlavor(Flavor flavor) { //Adds value to the slider
-        FoodSlider slider = GetFlavor(flavor.name);
-        slider.flavor.total += flavor.total;
+    public void AddFlavor(string name, int total) { //Adds value to the slider
+        FoodSlider slider = GetFlavor(name);
+        slider.flavor.total = total;
         slider.slider.value = slider.flavor.total;
+        slider.text.text = slider.flavor.total.ToString();
     }
 
 }
