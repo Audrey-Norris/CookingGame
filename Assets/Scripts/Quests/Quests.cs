7a6749005e4d4f9b721bfc08dcbedc74645a5bad
Audrey Norris <61563331+Audrey-Norris@users.noreply.github.com>
2025-04-10 10:34:04 -0600

    New Scripts

--- Assets/Scripts/Quests/Quests.cs
+++ Assets/Scripts/Quests/Quests.cs
@@ -10,13 +10,29 @@ public class Quests : ScriptableObject
     [TextArea(15, 20)]
     public string questDescription;
 
-    public List<ItemList> itemsNeeded = new List<ItemList>();
+    public Food itemNeeded;
 
     public Perks[] foodPerks;
+
+    public Flavor[] flavorList = new Flavor[5];
 
     public bool isCompleted = false;
 
     public bool isActive = false;
 
-    public Buildings buildingReward;
+    public Quests() {
+
+        this.flavorList[0].name = "Spicy";
+        this.flavorList[1].name = "Sweet";
+        this.flavorList[2].name = "Bitter";
+        this.flavorList[3].name = "Sour";
+        this.flavorList[4].name = "Umami";
+
+        for (int i = 0; i < 5; i++) {
+            this.flavorList[i].total = 0;
+        }
+
+
+
+    }
 }
